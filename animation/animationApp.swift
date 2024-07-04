036b685d153fc5f23b90eceb9c777ebf026ade71
HikaruHonda <hikaru.h.developer@gmail.com>
2024-07-04 22:33:11 +0900

    Initial Commit

---
+++ animation/animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by 本田輝 on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
